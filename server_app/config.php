839783ed09ecbc1cb068fef5385787cc8bd2a717
modul8 <sean@seanscouch.com>
2026-02-01 20:26:53 +1000

    Ignore config.php and sanitize sample config

--- server_app/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    "db_host" => "localhost",
-    "db_name" => "andrewdillon_wcapp",
-    "db_user" => "andrewdillon_appuser",
-    "db_pass" => "S3an9501!!",
-    "api_key" => "aec802f9415ad757db4630a286b9521f8ffa3e3cbb52f178b7b3ff54410c8391",
-];
--- server_app/config.sample.php
+++ server_app/config.sample.php
@@ -1,8 +1,8 @@
 <?php
 return [
     "db_host" => "localhost",
-    "db_name" => "andrewdillon_wcapp",
-    "db_user" => "andrewdillon_appuser",
+    "db_name" => "ils_app_db",
+    "db_user" => "ils_app_user",
     "db_pass" => "PUT_YOUR_DB_PASSWORD_HERE",
     "api_key" => "CHANGE_THIS_API_KEY",
     "ui_password" => "CHANGE_THIS_UI_PASSWORD",
