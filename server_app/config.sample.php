f614816888cc68deabdf69105d6ebb1fc4468e6b
modul8 <sean@seanscouch.com>
2026-02-02 18:58:30 +1000

    Add db_port sample and spray list with pins

--- server_app/config.sample.php
+++ server_app/config.sample.php
@@ -1,6 +1,7 @@
 <?php
 return [
     "db_host" => "localhost",
+    "db_port" => "3306",
     "db_name" => "ils_app_db",
     "db_user" => "ils_app_user",
     "db_pass" => "PUT_YOUR_DB_PASSWORD_HERE",
