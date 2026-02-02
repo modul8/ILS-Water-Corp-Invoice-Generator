1a6c193b966ab0f78c0d31c7970ad977ffa86044
modul8 <sean@seanscouch.com>
2026-02-02 19:51:42 +1000

    Deploy: sync assets separately

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,9 @@ rsync -av --delete \
   --exclude 'assets/' \
   "$SRC_DIR" \
   "$DEST_DIR"
+
+# Sync assets separately so upgrades can refresh them while still keeping config.php.
+rsync -av --delete \
+  --no-times --omit-dir-times --no-perms --no-owner --no-group \
+  "$SRC_DIR/assets/" \
+  "$DEST_DIR/assets/"
