0d3efc3fbf0827d910eaeb91978e73214fd7bcac
modul8 <sean@seanscouch.com>
2026-02-02 11:37:42 +1000

    Exclude assets and avoid dir times in deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ SRC_DIR="/mnt/evo-pool/apps/ils_app_repo/server_app/"
 DEST_DIR="/mnt/evo-pool/apps/ils_app/"
 
 rsync -av --delete \
-  --no-times --no-perms --no-owner --no-group \
+  --no-times --omit-dir-times --no-perms --no-owner --no-group \
   --exclude 'config.php' \
+  --exclude 'assets/' \
   "$SRC_DIR" \
   "$DEST_DIR"
